4a10090cda9e46d2f9de9ad2ae7d60082278616b
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-05 22:08:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,8 +3,7 @@ def map_to_negativize(array)
   i = 0
   
   while i < array.length 
-    if array[i] > 0
-      new_arr << array[i] * -1
+    new_arr << array[i] * -1
     else
       new_arr << array[i] 
     end
